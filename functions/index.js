eac03bd8c7e4c6786b947b1b8597f70e8cc7b2bc
Jacob Bashista <jacob@orange.haus>
2019-10-22 13:37:17 -0400

    Updated function

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,15 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.newpoint = functions.https.onRequest((request, response) => {
-    const lat = request.query.lat;
-    const long = request.query.long;
-    const time = request.query.time;
+    const data_body = request.body;
 
-    admin.database().ref('points').push({latitude: lat, longitude: long, timestamp:time});
+    const transmit_time = data_body.transmit_time;
+    const payload_data = data_body.data;
+    const iridium_latitude = data_body.iridium_latitude;
+    const iridium_longitude = data_body.iridium_longitude;
+    const iridium_cep = data_body.iridium_cep;
+
+    admin.database().ref('points').push({timestamp: transmit_time, data: payload_data, modem_lat: iridium_latitude, modem_long: iridium_longitude, accuracy: iridium_cep});
 
     response.status(200).send("Point Created!");
-    
 });
